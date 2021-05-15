c5925091c60c53ce16fb24c300a6b22d9b8a7e5c
Mani Moayedi <you@example.com>
2021-05-15 08:39:38 +0430

    Test

---
+++ CoreDaemon.Playground/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using CoreDaemon.Models;
+
+namespace CoreDaemon.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(Assembly.GetEntryAssembly()?.Location);
+            
+            Console.WriteLine(Assembly.GetEntryAssembly()?.CodeBase);
+            
+            Console.WriteLine(Assembly.GetEntryAssembly()?.FullName);
+            
+            Console.WriteLine(Assembly.GetEntryAssembly()?.EntryPoint?.Name);
+
+            Console.WriteLine("-----------------------------------------------");
+
+            ApplicationInfo info = Damien.Summon().GetApplicationInfo();
+
+            Console.WriteLine("ApplicationName: " + info.ApplicationName);
+            
+            Console.WriteLine("BinaryDirectory: " + info.BinaryDirectory);
+            
+            Console.WriteLine("ServiceName: " + info.ServiceName);
+
+            Damien.Summon().ExecuteCommands(new string[] {"daemon", "install"});
+        }
+    }
+}
